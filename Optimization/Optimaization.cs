c4f6509691dcb29c5178706c3f07ba04a6f0eb35
Ahmed Saleh <a.m0507979@gmail.com>
2026-03-05 19:21:59 +0200

    Benchmark for vs foreach; remove Lee algorithm demo
    Replaced Lee algorithm demo in Program.cs with a new Optimization.ForVsForeach() method that benchmarks and compares the performance of for and foreach loops over a large list. Added the Optimization class to encapsulate this benchmarking logic. Updated namespace and class structure accordingly.

---
+++ Optimization/Optimaization.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace LearnCS
+{
+    internal class Optimization
+    {
+        static List<int> elements = [];
+        static List<int> list1 = [];
+        static List<int> list2 = [];
+        public static void ForVsForeach()
+        {
+            for (int i = 0; i < 20000000; i++)
+            {
+                elements.Add(i);
+            }
+            Stopwatch sw = new();
+            sw.Start();
+            for (int i = 0; i < elements.Count; i++)
+            {
+                list1.Add(i);
+            }
+            sw.Stop();
+
+            Console.WriteLine($"For loop: {sw.ElapsedMilliseconds} ms");
+            sw.Restart();
+            foreach (var item in elements)
+            {
+                list2.Add(item);
+            }
+            sw.Stop();
+            Console.WriteLine($"Foreach loop: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
-﻿using LearnCS.Algorithms.ShortestPath;
-
-namespace LearnCS
+﻿namespace LearnCS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Lee.Run(new int[][] {
-                new int[] { 1, 0, 1, 1, 1 },
-                new int[] { 1, 0, 0, 0, 1 },
-                new int[] { 1, 1, 1, 0, 1 },
-                new int[] { 0, 0, 0, 0, 1 },
-                new int[] { 1, 1, 1, 1, 1 }
-            }, 0, 0, 4, 4);
+            Optimization.ForVsForeach();
         }
     }
 }
